144b8baa33ab14257b8b52fe9d5072b8909e8973
merijn florie <merijn@florie.nl>
2023-11-13 22:06:00 +0100

    base game done

---
+++ HUGamejam/Assets/Scripts/LightFlicker.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+[RequireComponent(typeof(Light2D))]
+public class LightFlicker : MonoBehaviour
+{
+    [SerializeField] private Light light;
+
+    private int frames = 0;
+
+    [SerializeField] private int framesPerRandomize;
+
+    [SerializeField] private float minValue;
+    [SerializeField] private float maxValue;
+
+
+    void Update()
+    {
+        frames++;
+        if (frames % framesPerRandomize == 0)
+        {
+            RandomizeIntensity();
+        }
+    }
+
+    void RandomizeIntensity()
+    {
+        // Create an instance of the Random class
+        System.Random random = new System.Random();
+
+
+        float randomValue = (float)(random.NextDouble() * (maxValue - minValue) + minValue);
+
+        light.intensity = randomValue;
+    }
+}
